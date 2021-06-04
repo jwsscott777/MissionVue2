8d59637f1f0f7732bdb6ab8255c05bf048b8e72d
James Scott <tlmsols50@gmail.com>
2021-06-04 09:05:10 -0400

    Add json

---
+++ src/components/EventCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <router-link to="/event/5928101">
+    <div class="event-card">
+      <span>@ {{ event.time }} on {{ event.date }}</span>
+      <h4>{{ event.title }}</h4>
+    </div>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: "EventCard",
+  props: {
+    event: Object
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  background-color: rgb(230, 135, 47);
+  border: 1px solid #39495c;
+  border-radius: 25px;
+  margin-bottom: 18px;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+.event-link {
+  color: #0c76e0;
+  text-decoration: none;
+}
+</style>
---
+++ src/views/EventDetails.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Test</h1>
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
